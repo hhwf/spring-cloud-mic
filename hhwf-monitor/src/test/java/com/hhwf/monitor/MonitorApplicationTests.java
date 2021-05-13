7fc384e87fa91b3c1cca39373d894b7ea944d028
hhwf <329589951@qq.com>
2021-05-13 13:24:30 +0800

    hhwf-monitor

---
+++ hhwf-monitor/src/test/java/com/hhwf/monitor/MonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhwf.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
